525ee24c5e3d6183643b98dbe42a9e515c671e30
NAQVI <nomanalam7214@gmail.com>
2019-08-01 13:01:56 +0200

    added

---
+++ build/precache-manifest.b716b04b7e40ee2cd2137177dcf10c75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421f09edefafc8732c4a15be95398373",
+    "url": "/nrcool/react-calculator/index.html"
+  },
+  {
+    "revision": "cb01a8acdd49fff2db5c",
+    "url": "/nrcool/react-calculator/static/css/main.37b9cba0.chunk.css"
+  },
+  {
+    "revision": "5ce3850e715969cf19e5",
+    "url": "/nrcool/react-calculator/static/js/2.141dd15f.chunk.js"
+  },
+  {
+    "revision": "cb01a8acdd49fff2db5c",
+    "url": "/nrcool/react-calculator/static/js/main.7c03e2c7.chunk.js"
+  },
+  {
+    "revision": "491e01400e354c4710b8",
+    "url": "/nrcool/react-calculator/static/js/runtime~main.04202d6a.js"
+  }
+]);
